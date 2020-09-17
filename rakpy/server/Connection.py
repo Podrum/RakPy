34faec765f65f616dd8dd1eb7d38f275a594660c
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-18 00:45:08 +0300

    Update Connection.py

--- rakpy/server/Connection.py
+++ rakpy/server/Connection.py
@@ -10,3 +10,11 @@ from rakpy.protocol.EncapsulatedPacket import EncapsulatedPacket
 from rakpy.protocol.Nack import Nack
 from rakpy.protocol.NewIncomingConnection import NewIncomingConnection
 from rakpy.utils.InternetAddress import InternetAddress
+
+class Connection:
+    priorityNormal = 0
+    priorityImmediate = 1
+    statusConnecting = 0
+    statusConnected = 1
+    statusDisconnecting = 2
+    statusDisconnected = 3
