4f72e7c0a17015b299b222e7b55f03094944b87d
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-18 01:03:22 +0300

    Update Connection.py

--- rakpy/server/Connection.py
+++ rakpy/server/Connection.py
@@ -10,11 +10,45 @@ from rakpy.protocol.EncapsulatedPacket import EncapsulatedPacket
 from rakpy.protocol.Nack import Nack
 from rakpy.protocol.NewIncomingConnection import NewIncomingConnection
 from rakpy.utils.InternetAddress import InternetAddress
+from time import time as timeNow
 
 class Connection:
-    priorityNormal = 0
-    priorityImmediate = 1
-    statusConnecting = 0
-    statusConnected = 1
-    statusDisconnecting = 2
-    statusDisconnected = 3
+    priority = {
+        "Normal": 0,
+        "Immediate": 1
+    }
+
+    status = {
+        "Connecting": 0,
+        "Connected": 1,
+        "Disconnecting": 2,
+        "Disconnected": 3
+    }
+    
+    listener = None
+    mtuSize = None
+    address = None
+    state = status["Connecting"]
+    nackQueue = []
+    ackQueue = []
+    recoveryQueue = {}
+    packetToSend = []
+    sendQueue = DataPacket()
+    splitPackets = []
+    windowStart = -1
+    windowEnd = 2048
+    reliableWindowStart = 0
+    reliableWindowEnd = 2048
+    reliableWindow = {}
+    lastReliableIndex = -1
+    receivedWindow = []
+    lastSequenceNumber = -1
+    sendSequenceNumber = 0
+    messageIndex = 0
+    channelIndex = {}
+    needACK = []
+    splitID = 0
+    lastUpdate = int(timeNow())
+    isActive = False
+    
+    
