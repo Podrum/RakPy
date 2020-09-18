7c1e905313e5d7eb485cec6c469dfb7fe6d48f3e
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-18 20:04:37 +0300

    Update Connection.py

--- rakpy/server/Connection.py
+++ rakpy/server/Connection.py
@@ -128,5 +128,5 @@ class Connection:
             return
         elif dataPacket.sequenceNumber > self.windowEnd:
             return
-        elif self.receivedWindow.includes(dataPacket.sequenceNumber):
+        elif self.receivedWindow.has_key(dataPacket.sequenceNumber):
             return
