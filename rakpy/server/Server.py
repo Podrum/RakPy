98675e88af6870b50aa1809611b08dcd8b8dd23c
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-26 08:18:52 +0300

    Update Server.py

--- rakpy/server/Server.py
+++ rakpy/server/Server.py
@@ -1,9 +1,40 @@
 from binutilspy.Binary import Binary
+from rakpy.server.ServerSocket import ServerSocket
+from rakpy.utils.InternetAddress import InternetAddress
 import os
+from threading import Thread
 
-class Server:
+class Server(Thread):
+    protocol = 10
+    raknetTps = 100
+    raknetTickLength = 1 / raknetTps
+    
     id = Binary.readLong(os.urandom(8))
     name = None
     socket = None
     connections = {}
     shutdown = False   
+    
+    def __init__(self, address):
+        super().__init__()
+        self.socket = ServerSocket(address)
+        self.listen(address)
+        self.start()
+        
+    def handle(self, data, address):
+        header = data[0]
+        token = f"{address.getAddress}:{address.getPort}"
+        if token in self.connections:
+            connection = self.connections[token]
+            connection.receive(data)
+        else:
+            if header == PacketIdentifiers.UnconnectedPing:
+                socket.sendBuffer(self.handleUnconnectedPing(data), (address.getAddress(), address.getPort()))
+            elif header == PacketIdentifiers.OpenConnectionRequest1:
+                pass
+        
+    def run(self):
+        while True:
+            if self.socket.getPacket() != None:
+                data, address = self.socket.getPacket()
+                self.handle(data, InternetAddress(address[0], address[1]))
