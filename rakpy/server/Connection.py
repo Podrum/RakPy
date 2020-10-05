a77808c05a7031a82f79c9d419167d8218d1fab6
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-10-05 08:33:55 -0500

    Convert to is none

--- rakpy/server/Connection.py
+++ rakpy/server/Connection.py
@@ -175,7 +175,7 @@ class Connection:
                 del self.recoveryQueue[seq]
                 
     def receivePacket(self, packet):
-        if packet.messageIndex == None:
+        if packet.messageIndex is None:
             self.handlePacket(packet)
         else:
             if packet.messageIndex < self.reliableWindowStart:
