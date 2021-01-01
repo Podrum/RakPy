8132b4e0e97d2b4fa91062b8f01c65ac3f52357a
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-01-01 06:08:35 +0200

    Update ServerSocket.py

--- rakpy/server/ServerSocket.py
+++ rakpy/server/ServerSocket.py
@@ -1,18 +1,18 @@
 import socket
 
-class ServerSocket:
+class Socket:
     socket = None
     address = None
     
     def __init__(self, address):
         self.address = address
-        self.socket = socket.socket(socket.AF_INET if address.getVersion() == 4 else socket.AF_INET6, socket.SOCK_DGRAM, socket.SOL_UDP)
-        if address.getVersion() == 6:
+        self.socket = socket.socket(socket.AF_INET if address.version == 4 else socket.AF_INET6, socket.SOCK_DGRAM, socket.SOL_UDP)
+        if address.version == 6:
             self.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 1)
         try:
-            self.socket.bind((self.address.getAddress(), self.address.getPort()))
+            self.socket.bind((self.address.address, self.address.port))
         except socket.error as e:
-            print(f"Unable to binto to {str(address.getPort())}")
+            print(f"Unable to binto to {str(address.port)}")
             print(str(e))
         else:
             self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
