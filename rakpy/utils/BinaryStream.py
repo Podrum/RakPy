9c77d2b935db62ae46ba50c2a4273483ee8656c2
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-01-01 05:20:54 +0200

    Woops

--- rakpy/utils/BinaryStream.py
+++ rakpy/utils/BinaryStream.py
@@ -36,7 +36,7 @@ class BinaryStream:
     def getSignedShort(self):
         return struct.unpack(">h", self.get(2))[0]
     
-    def putShort(self, value)
+    def putShort(self, value):
         self.put(struct.pack(">H", value))
         
     def getLShort(self):
@@ -45,29 +45,29 @@ class BinaryStream:
     def getSignedLShort(self):
         return struct.unpack("<h", self.get(2))[0]
     
-    def putLShort(self, value)
+    def putLShort(self, value):
         self.put(struct.pack("<h", value))
         
     def getTriad(self):
         return struct.unpack(">i", b"\x00" + self.get(3))[0]
     
-    def putTriad(self, value)
+    def putTriad(self, value):
         self.put(struct.pack(">i", value)[1:)
         
     def getLTriad(self):
         return struct.unpack("<I", self.get(3) + b"\x00")[0]
     
-    def putLTriad(self, value)
+    def putLTriad(self, value):
         self.put(struct.pack("<I", value)[:-1])
         
     def getInt(self):
         return struct.unpack(">i", self.get(4))[0]
     
-    def putInt(self, value)
+    def putInt(self, value):
         self.put(struct.pack(">i", value))
         
     def getLInt(self):
         return struct.unpack("<i", self.get(4))[0]
     
-    def putLInt(self, value)
+    def putLInt(self, value):
         self.put(struct.pack("<i", value))
