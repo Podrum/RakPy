ececd477a937d392d09c9590e8fca6e418ea6d1f
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-02-01 01:32:24 +0200

    Update UnconnectedPing.py

--- rakpy/protocol/UnconnectedPing.py
+++ rakpy/protocol/UnconnectedPing.py
@@ -3,9 +3,7 @@ from rakpy.protocol.PacketIdentifiers import PacketIdentifiers
 
 class UnconnectedPing(OfflinePacket):
     id = PacketIdentifiers.UnconnectedPing
-    
     time = 0
-    clientId = 0
     
     def encodePayload(self):
         self.putLong(self.time)
